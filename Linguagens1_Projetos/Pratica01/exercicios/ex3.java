94c3ed8e92a96f27ca1762ea00c64e08815ccf17
ThiagoTomoyuki <thiagotomoyuki@hotmail.com>
2020-03-04 11:16:57 -0300

    Coreção ex3.

--- Linguagens1_Projetos/Pratica01/exercicios/ex3.java
+++ Linguagens1_Projetos/Pratica01/exercicios/ex3.java
@@ -12,7 +12,7 @@ public class ex3{
         usuario1 = scanner.nextLine();
         System.out.print("Informe a senha1: ");
         senha1 = scanner.nextLine();
-        while((senha1 != senha2) && (usuario1 != usuario2)){
+        while(!(senha1.equals(senha2)) && !(usuario1.equals(usuario2))){
             System.out.print("Informe o usuario2: ");
             usuario2 = scanner.nextLine();
             System.out.print("Informe a senha2: ");
