1faa95b6f770016f149108b1037648a1dc8deefc
ThiagoTomoyuki <thiagotomoyuki@hotmail.com>
2020-03-23 11:01:24 -0300

    aula3

---
+++ Linguagens1_Projetos/Pratica03/aula/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       Conta c1 = new Conta(500);
+       System.out.println("Saldo Atual: R$" + c1.consultaSaldo());
+       c1.depositar(1000);
+        System.out.println("Saldo Atual: R$" + c1.consultaSaldo());
+        c1.sacar(200);
+        System.out.println("Saldo Atual: R$" + c1.consultaSaldo());
+        c1.depositar(1200);
+        System.out.println("Saldo Atual: R$" + c1.consultaSaldo());
+    }
+}
