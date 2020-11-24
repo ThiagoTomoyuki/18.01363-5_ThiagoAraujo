983e039da0b2a7a1dcc63831f8af82787a4c5677
ThiagoTomoyuki <thiagotomoyuki@hotmail.com>
2020-11-24 10:55:55 -0300

    Criação da classe personagem

---
+++ Linguagens1_Projetos/Atividade_4/src/models/Personagem.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Personagem {
+    String nome;
+    Enum raca,prof;
+    int mana;
+    double ataque,ataque_magico,defesa,defesa_magica,velocidade,destreza;
+    int exp,nivel_atual;
+
+    public Personagem(String nome,){
+
+    }
+
+}
