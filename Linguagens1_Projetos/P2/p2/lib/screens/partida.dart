9c2fdd305a789e81bb42ba1acbdca615d3eadce8
ThiagoTomoyuki <61150933+ThiagoTomoyuki@users.noreply.github.com>
2020-12-02 23:01:10 -0300

    final

--- Linguagens1_Projetos/P2/p2/lib/screens/partida.dart
+++ Linguagens1_Projetos/P2/p2/lib/screens/partida.dart
@@ -1,51 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:p2/models/Account.dart';
 import 'package:p2/models/Matches.dart';
+import 'package:p2/models/Partida2.dart';
 
 // ignore: must_be_immutable
 class Partida extends StatelessWidget {
   List<Matche> allMatches;
-  Account account = new Account();
-  Matches matches = new Matches();
-  Partida(this.allMatches);
+  String nomeInvocador;
+  Partida2 partida = new Partida2();
+  Partida({Key key,@required this.partida, @required this.nomeInvocador}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    print(allMatches[0].champion);
-    
+    print("Cheguei");
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(title:Text("Historico de partidas"),backgroundColor: Colors.blueAccent),
       body:Column(
         children: [
           SizedBox(width:300,height: 200,child:Image.asset("assets/dices/logoLOL.png")),
-          Text("Partidas recentes (Últimas 15 jogadas)",style:TextStyle(fontSize: 20)),
+          // Text("Partidas recentes (Últimas 15 jogadas) de "+nomeInvocador,style:TextStyle(fontSize: 20)),
+          Text("Última Partida jogada por "+nomeInvocador,style:TextStyle(fontSize: 30)),
           Expanded(
             child: Row(children: [
-              Column(
+              Center(child: Column(
                 children:[
-                  Text("Nome campeao"),
-                  SizedBox(width:100,height: 50,child: Image.network("https://pyre-media-2.s3.sa-east-1.amazonaws.com/1fc06b12-a8b8-4dbb-a692-9111bc084e7cleague-of-legends-maracanazinho.png"))
-                ]
-              ),
-              Column(
-                children:[
-                  Text("KDA"),
+                  Text("Duração do jogo(em segundos): "+partida.gameDuration.toString(),style: TextStyle(fontSize: 25, color: Colors.black)),
+                  Text("O modo do jogo foi: "+partida.gameMode.toString(),style: TextStyle(fontSize: 25, color: Colors.black)),
+                  Text("O tipo do jogo foi: "+partida.gameType.toString(),style: TextStyle(fontSize: 25, color: Colors.black)),
+                  Text("O time azul: "+partida.teams[0].win.toString(),style: TextStyle(fontSize: 25, color: Colors.black)),
+                  Text("O time vermelho: "+partida.teams[1].win.toString(),style: TextStyle(fontSize: 25, color: Colors.black)),
                   
                 ]
               ),
-              Column(
-                children:[
-                  Text("DATA"),
-                ]
               ),
-              Column(
-                children:[
-                  Text("WON"),
-                ]
-              ),
-              
-
-            ],),
+            ]
+            ),
           )
         ]
       )
